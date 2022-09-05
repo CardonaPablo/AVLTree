4e5da4df3b6fdd6c8dac2c91939e453973b0c010
Pablo Cardona <pablo@loroinsurance.com>
2022-09-05 13:12:19 -0500

    Method to draw tree LL Rotation Finished detect imbalance function

--- Tree/Tree.hpp
+++ Tree/Tree.hpp
@@ -5,6 +5,7 @@ template <typename T>
 class Node;
 
 #include <iostream>
+#include <cstring>
 
 template <typename T> 
 class Tree {
@@ -28,30 +29,91 @@ class Tree {
 
 		void balance() {
 			// Follow the path with max height 3 levels to find rotation
-			char route[3] = { 'N' , 'N', 'N' }; 
-			// std::cout << "Checking balance for " << std::endl;
+			char route[2] = { 'N' , 'N' }; 
+			std::cout << "Checking balance for " << std::endl;
 			root -> printNode();
-			// std::cout << std::endl;
+			std::cout << std::endl;
 			Node<T>* selected = root;
-			for (int i = 0; i < 3; i++)
+			for (int i = 0; i < 2; i++)
 			{
-				if(selected->left == nullptr || selected->right->height  > selected->left->height) {
+				int lHeight = 0;
+				int rHeight = 0;
+				if(selected->left != nullptr)
+					lHeight = selected->left->height;
+				if(selected->right != nullptr)
+					rHeight = selected->right->height;
+
+				if(selected->left == nullptr || rHeight  > lHeight) {
 					route[i] = 'R';
 					selected = root->right;
 				}
-				if(selected->right == nullptr || selected->left->height  > selected->right->height) {
+				if(selected->right == 0 || lHeight  > rHeight) {
 					route[i] = 'L';
 					selected = root->left;
 				}
-				// std::cout << "Determining imbalance: Level " << i << " is " << route[i] << std::endl;
 			}
 
-			std::cout << "Detected imbalance" << std::endl;
-			for (int j = 0; j < 3; j++)
-			{
-				std::cout << route[j];
+			if(strcmp(route, "LL") == 0) {
+				std::cout << "Detected " << route << " imbalance" << std::endl;
+				rotationLL();
 			}
-			std::cout << std::endl;
+			// } else if
+
+		}
+
+		void drawTree() {
+			std::cout << "\t\t" << root->getData() << std::endl;
+			if(root->left != nullptr)
+				std::cout << "\t" << root->left->getData();
+			else
+				std::cout << "NULL";
+
+			std::cout << "\t\t";
+
+			if(root->right != nullptr)
+				std::cout << root->right->getData() << std::endl;
+			else
+				std::cout << "NULL" << std::endl;
+
+			// Draw left children
+			if(root->left != nullptr && root->left->left != nullptr)
+				std::cout << root->left->left->getData() << "\t";
+			else
+				std::cout << "NULL\t";
+
+			if(root->left != nullptr && root->left->right != nullptr)
+				std::cout << root->left->right->getData() << "\t\t";
+			else
+				std::cout << "NULL\t\t";
+
+			// Draw right children
+			if(root->right != nullptr && root->right->left != nullptr)
+				std::cout << root->right->left->getData() << "\t";
+			else
+				std::cout << "NULL" << "\t";
+
+			if(root->right != nullptr && root->right->right != nullptr)
+				std::cout << root->right->right->getData();
+			else
+				std::cout << "NULL";
+			
+			std::cout << std::endl << "---------------------------------------------" << std::endl;
+		}
+
+
+
+		void rotationLL() {
+			std::cout << "Performing LL rotation" << std::endl;
+			drawTree();
+			// Guardar raíz→L→ R 
+			Node<T>* buffer = root->left->right;
+			// Copiar el puntero de raíz hacia la raíz→L→R
+			root->left->right= root;
+			// Hacer raíz→L la nueva raíz
+			root = root->left;
+			// Reemplazar raíz→R→L con el valor guardado en (1)
+			root->right->left = buffer;
+			drawTree();
 		}
 
 };
--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@
 
 int main() {
 	Node<int> root;
-	root.setData(60);
+	// root.setData(60);
+	// root.push(20);
+	// root.push(70);
+	// root.push(10);
+	// root.push(40);
+	// root.push(30);
+	// root.push(50);
+	root.setData(30);
 	root.push(20);
-	root.push(70);
 	root.push(10);
-	root.push(40);
-	root.push(30);
-	root.push(50);
+	// root.push(40);
+	// root.push(30);
+	// root.push(50);
 	// std::cout << "PreOrder" << std::endl;
 	// root.preOrder();
 	// std::cout << std::endl << "InOrder" << std::endl;
